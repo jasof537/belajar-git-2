5304345692b14c86a6a8374fdeaf52bf24275797
jasof537 <jajangsofian.2113177026@gmail.com>
2024-04-28 12:43:40 +0700

    penambahan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla rerum molestiae atque dolorem rem aut eaque debitis! Impedit sequi culpa, aliquid mollitia iusto soluta incidunt totam ipsam aspernatur saepe!</p>
+    <ul>
+        <li>Abcd</li>
+    </ul>
 </body>
 </html>
